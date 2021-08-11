3340cabad27904379200cb1c3fc2277f2f37adf7
Kade Griffith <kadeg95@gmail.com>
2021-08-11 12:13:44 -0600

    first commit. added api request and set ddata to the response

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import { BASE_URL, API_KEY } from "./constants/Constants";
+import Details from "./details/details"
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect( () => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then( res => setData(res.data))
+      .catch(err => {
+        console.error(err);
+        setError(err)
+      })
+  }, [])
+
+  useEffect( () => {
+    console.log(data);
+  }, [data])
+  // data.map( item => {
+  //   return <div>
+      
+  //   </div>
+  // })
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className="app">
+      {error && <h1>{error}</h1>}
+      <h1>APOD</h1>
+      <Details data={data}/>
+
     </div>
   );
 }
---
+++ src/details/details.js
@@ -0,0 +1,7 @@
+import React, {useState, useEffect} from "react";
+
+const Details = () => {
+    return null;
+}
+
+export default Details;
