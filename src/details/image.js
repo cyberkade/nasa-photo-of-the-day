e2e961d4a81e7003532002fcb35089694df21e64
Kade Griffith <kadeg95@gmail.com>
2022-03-29 13:35:47 -0600

    slight animation change

--- src/details/image.js
+++ src/details/image.js
@@ -1,32 +1,49 @@
 import React from "react";
-import styled from 'styled-components';
+import styled, { keyframes } from "styled-components";
+
+const kf = keyframes`
+        50%{
+        transform: scale(0.9);
+        }
+        100%{
+        opacity: 1;
+        transform: scale(1);
+        }
+    `;
 
 const StyledDiv = styled.div`
-    width: 50vw; height: 100%;
-    text-align: center;
-    border-radius: 25px;
-    padding: 15px 15px;
-`
+  animation: ${kf} 0.6s ease-in-out forwards;
+  width: 50vw;
+  height: 100%;
+  text-align: center;
+  border-radius: 25px;
+  padding: 15px 15px;
+`;
 
 const StyledImg = styled.img`
-    width: 634px; height: 100%;
-    border-radius: 25px;
-    align-self: center;
-`
+  width: 634px;
+  height: 100%;
+  border-radius: 25px;
+  align-self: center;
+`;
 const Image = (props) => {
-    const {mediaType, APOD} = props;
+  const { mediaType, APOD } = props;
 
-    if(mediaType === 'image'){
-        return(
-            <StyledDiv>
-            <StyledImg src={APOD} alt="astronomy pic of the day"></StyledImg>
-            </StyledDiv>
-        )
-    } else {return(
-            <div> Turns out the Astronomy Picture of the Day wasn't a picture! Crazy, right? Come back tomorrow! </div>  
-    )
-    
-    }
-}
+  if (mediaType === "image") {
+    return (
+      <StyledDiv>
+        <StyledImg src={APOD} alt="astronomy pic of the day"></StyledImg>
+      </StyledDiv>
+    );
+  } else {
+    return (
+      <div>
+        {" "}
+        Turns out the Astronomy Picture of the Day wasn't a picture! Crazy,
+        right? Come back tomorrow!{" "}
+      </div>
+    );
+  }
+};
 
 export default Image;
--- src/details/info.js
+++ src/details/info.js
@@ -1,106 +1,102 @@
 import React from "react";
-import styled, {keyframes}  from 'styled-components';
-    
-    const kf = keyframes`
+import styled, { keyframes } from "styled-components";
+
+const kf = keyframes`
         50%{
-        transform: scale(0.8);
+        opacity: .3;
+        transform: scale(0.9);
         }
         100%{
         opacity: 1;
         transform: scale(1);
         }
-    `
+    `;
 
-    const StyledDiv = styled.div`
-        transform: scale(2);
-        opacity: 0;
-        animation: ${kf} 0.3s ease-in-out forwards;
-        padding: 25px;
-        display: flex;
-        flex-flow: column nowrap;
-        justify-content: center;
+const StyledDiv = styled.div`
+  transform: scale(2);
+  opacity: 0;
+  animation: ${kf} 0.6s ease-in-out forwards;
+  padding: 25px;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
 
-        h1 {
-            font-size:32px;
-            display: inline-block;
-            padding: 5px;
-            margin: 0px;
-            color:
-                ${props => props.type === 'materialDark' ? props.theme.materialTitle : null}
-                ${props => props.type === 'regular' ? props.theme.regularTitle : null}
-            ;
-        }
+  h1 {
+    font-size: 32px;
+    display: inline-block;
+    padding: 5px;
+    margin: 0px;
+    color: ${(props) =>
+        props.type === "materialDark" ? props.theme.materialTitle : null}
+      ${(props) => (props.type === "regular" ? props.theme.regularTitle : null)};
+  }
 
-        h2 {
-            padding: 5px;
-            margin: 0px 0px 0px 5px;
-            color:
-                ${props => props.type === 'materialDark' ? props.theme.materialDate : null}
-                ${props => props.type === 'regular' ? props.theme.regularDate : null}
-            ;
-          }
+  h2 {
+    padding: 5px;
+    margin: 0px 0px 0px 5px;
+    color: ${(props) =>
+        props.type === "materialDark" ? props.theme.materialDate : null}
+      ${(props) => (props.type === "regular" ? props.theme.regularDate : null)};
+  }
 
-        h3 {
-            padding: 5px;
-            margin: 10px 50px 0px 0px;
-            align-self: flex-end;
-            color:
-                ${props => props.type === 'materialDark' ? props.theme.materialDate : null}
-                ${props => props.type === 'regular' ? props.theme.regularDate : null}
-            ;
-          }
+  h3 {
+    padding: 5px;
+    margin: 10px 50px 0px 0px;
+    align-self: flex-end;
+    color: ${(props) =>
+        props.type === "materialDark" ? props.theme.materialDate : null}
+      ${(props) => (props.type === "regular" ? props.theme.regularDate : null)};
+  }
 
-        p {
-            padding: 0px;
-            color:
-                ${props => props.type === 'materialDark' ? props.theme.materialGray : null}
-                ${props => props.type === 'regular' ? props.theme.regularBlack : null}
-            ;
-        }
-
-
-    `
-    const StyledButton = styled.button`
-        align-self: flex-end;
-        display: inline-block;
-        border: none;
-        border-radius: 15px;
-        padding: 5px 10px;
-        margin: 0px 50px 0px 0px;
-        box-shadow:
-            ${props => props.type === 'materialDark' ? props.theme.materialBoxShadow : null}
-            ${props => props.type === 'regular' ? props.theme.regularBoxShadow : null}
-        ;
-        display: flex;
-        background-color: 
-            ${props => props.type === 'materialDark' ? props.theme.materialDark : null}
-            ${props => props.type === 'regular' ? props.theme.regular : null}
-        ;
-        color:
-            ${props => props.type === 'materialDark' ? props.theme.materialGray : null}
-            ${props => props.type === 'regular' ? props.theme.regularBlack : null}
-        ;
-        &:hover {
-            box-shadow:
-                ${props => props.type === 'materialDark' ? props.theme.materialBoxShadowHover : null}
-                ${props => props.type === 'regular' ? props.theme.regularBoxShadowHover : null}
-            ;
-        }
-
-    `
+  p {
+    padding: 0px;
+    color: ${(props) =>
+        props.type === "materialDark" ? props.theme.materialGray : null}
+      ${(props) => (props.type === "regular" ? props.theme.regularBlack : null)};
+  }
+`;
+const StyledButton = styled.button`
+  align-self: flex-end;
+  display: inline-block;
+  border: none;
+  border-radius: 15px;
+  padding: 5px 10px;
+  margin: 0px 50px 0px 0px;
+  box-shadow: ${(props) =>
+      props.type === "materialDark" ? props.theme.materialBoxShadow : null}
+    ${(props) =>
+      props.type === "regular" ? props.theme.regularBoxShadow : null};
+  display: flex;
+  background-color: ${(props) =>
+      props.type === "materialDark" ? props.theme.materialDark : null}
+    ${(props) => (props.type === "regular" ? props.theme.regular : null)};
+  color: ${(props) =>
+      props.type === "materialDark" ? props.theme.materialGray : null}
+    ${(props) => (props.type === "regular" ? props.theme.regularBlack : null)};
+  &:hover {
+    box-shadow: ${(props) =>
+        props.type === "materialDark"
+          ? props.theme.materialBoxShadowHover
+          : null}
+      ${(props) =>
+        props.type === "regular" ? props.theme.regularBoxShadowHover : null};
+  }
+`;
 
 const Info = (props) => {
-    const {explanation, title, date, switchTheme, theme, credit} = props;
+  const { explanation, title, date, switchTheme, theme, credit } = props;
 
-    return (
-        <StyledDiv type={theme} >
-            <h1 type={theme} >{title}</h1>
-            <StyledButton type={theme} onClick={() => switchTheme()}>Switch Theme!</StyledButton>
-            <h2>{date}</h2>
-            <p>{explanation}</p>
-            <h3>Photographer - {credit}</h3>
-        </StyledDiv>
-    )
-} 
+  return (
+    <StyledDiv type={theme}>
+      <h1 type={theme}>{title}</h1>
+      <StyledButton type={theme} onClick={() => switchTheme()}>
+        {`${theme === "regular" ? "Dark" : "Light"} Theme!`}
+      </StyledButton>
+      <h2>{date}</h2>
+      <p>{explanation}</p>
+      <h3>Photographer - {credit}</h3>
+    </StyledDiv>
+  );
+};
 
 export default Info;
